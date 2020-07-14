c386718d6cf2c1e465309efeff16b5642a29227e
dm13579 <dongming053021@163.com>
2020-07-14 17:09:31 +0800

    other

--- springcloud-ribbon-order/src/main/java/com/dm/springcloud/config/WebConfig.java
+++ springcloud-ribbon-order/src/main/java/com/dm/springcloud/config/WebConfig.java
@@ -1,5 +1,7 @@
 package com.dm.springcloud.config;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,5 +17,10 @@ public class WebConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public IRule randomRule(){
+        return new RandomRule();
     }
 }
