c341214d04ad859c44ada39e572defd92560321c
dm13579 <dongming053021@163.com>
2020-07-17 15:23:25 +0800

    sentinel持久化-push模式

---
+++ springcloud-sentinel-rulepersistence-push-order/src/main/java/com/dm/springcloud/SpringcloudSentinelRulepersistencePushOrder.java
@@ -0,0 +1,14 @@
+package com.dm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ */
+@SpringBootApplication
+public class SpringcloudSentinelRulepersistencePushOrder {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudSentinelRulepersistencePushOrder.class, args);
+    }
+}
