289748681c8dd2142246ae79f5c4343db82b26b4
dm13579 <dongming053021@163.com>
2020-07-17 11:37:36 +0800

    容错手段-超时

--- springcloud-timeout-order/src/main/java/com/dm/springcloud/controller/OrderInfoController.java
+++ springcloud-timeout-order/src/main/java/com/dm/springcloud/controller/OrderInfoController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * 订单Controller
+ */
 @RestController
 public class OrderInfoController {
 
--- springcloud-timeout-product/src/main/java/com/dm/springcloud/controller/ProductInfoController.java
+++ springcloud-timeout-product/src/main/java/com/dm/springcloud/controller/ProductInfoController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 商品Controller
+ */
 @RestController
 public class ProductInfoController {
 
